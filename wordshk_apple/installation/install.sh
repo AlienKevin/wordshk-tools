c92ea3602b180bd18e836e6524125028a13b8c8f
Kevin Li <kevinli020508@gmail.com>
2021-11-21 12:42:52 -0500

    Remove unnecessary install script and dictionary file

--- wordshk_apple/installation/install.sh
+++
@@ -1,9 +0,0 @@
-DICT_NAME="wordshk"
-DESTINATION_FOLDER=~/Library/Dictionaries
-
-echo "Installing into $DESTINATION_FOLDER".
-mkdir -p $DESTINATION_FOLDER
-ditto --noextattr --norsrc ./$DICT_NAME.dictionary  $DESTINATION_FOLDER/$DICT_NAME.dictionary
-touch $DESTINATION_FOLDER
-echo "Done."
-echo "To test the new dictionary, try Dictionary.app."
