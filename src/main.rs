a825b2020e2ef4e2130c88e91a49c6a4974d52da
Kevin Li <kevinli020508@gmail.com>
2021-12-24 20:46:10 -0500

    Improve do_pr_search() test cases and outputs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::process;
-use wordshk_tools::emit::prs_to_string;
+use wordshk_tools::emit::{pr_to_string, prs_to_string};
 use wordshk_tools::emit_apple_dict::dict_to_xml;
 use wordshk_tools::parse::{
-    parse_dict, JyutPing, JyutPingCoda, JyutPingInitial, JyutPingNucleus, LaxJyutPingSegment,
+    parse_dict, JyutPing, JyutPingCoda, JyutPingInitial, JyutPingNucleus, JyutPingTone,
+    LaxJyutPingSegment,
 };
 use wordshk_tools::search::{pr_search, variant_search, PrSearchResult, VariantSearchResult};
 
 fn main() {
-    do_variant_search();
+    do_pr_search();
 }
 
 fn do_variant_search() {
@@ -41,34 +42,66 @@ fn do_pr_search() {
             process::exit(1);
         }
         Ok(dict) => {
-            // test 麪包 jyutping search with slight typos
-            let mut results = pr_search(
-                &dict,
-                &vec![
-                    LaxJyutPingSegment::Standard(JyutPing {
-                        initial: Some(JyutPingInitial::M),
-                        nucleus: JyutPingNucleus::I,
-                        coda: Some(JyutPingCoda::Ng),
-                        tone: None,
-                    }),
-                    LaxJyutPingSegment::Standard(JyutPing {
-                        initial: Some(JyutPingInitial::B),
-                        nucleus: JyutPingNucleus::Aa,
-                        coda: Some(JyutPingCoda::U),
-                        tone: None,
-                    }),
-                ],
-            );
-            let mut i = 0;
-            while results.len() > 0 && i < 10 {
-                let PrSearchResult {
-                    id, variant_index, ..
-                } = results.pop().unwrap();
-                let entry = dict.get(&id).unwrap();
-                let variant = &entry.variants[variant_index];
-                println!("{} {}", variant.word, prs_to_string(&variant.prs));
-                i += 1;
-            }
+            let queries = vec![
+                (
+                    "麪包",
+                    vec![
+                        LaxJyutPingSegment::Standard(JyutPing {
+                            initial: Some(JyutPingInitial::M),
+                            nucleus: JyutPingNucleus::I,
+                            coda: Some(JyutPingCoda::Ng),
+                            tone: None,
+                        }),
+                        LaxJyutPingSegment::Standard(JyutPing {
+                            initial: Some(JyutPingInitial::B),
+                            nucleus: JyutPingNucleus::Aa,
+                            coda: Some(JyutPingCoda::U),
+                            tone: None,
+                        }),
+                    ],
+                ),
+                (
+                    "學生",
+                    vec![
+                        LaxJyutPingSegment::Standard(JyutPing {
+                            initial: Some(JyutPingInitial::H),
+                            nucleus: JyutPingNucleus::O,
+                            coda: Some(JyutPingCoda::K),
+                            tone: Some(JyutPingTone::T6),
+                        }),
+                        LaxJyutPingSegment::Standard(JyutPing {
+                            initial: Some(JyutPingInitial::S),
+                            nucleus: JyutPingNucleus::Aa,
+                            coda: Some(JyutPingCoda::Ng),
+                            tone: None,
+                        }),
+                    ],
+                ),
+            ];
+            queries.iter().for_each(|(intended, query)| {
+                println!("{}\t{}\n", intended, pr_to_string(query));
+                let mut results = pr_search(&dict, query);
+                let mut i = 0;
+                while results.len() > 0 && i < 10 {
+                    let PrSearchResult {
+                        id,
+                        variant_index,
+                        pr_index,
+                        score,
+                        ..
+                    } = results.pop().unwrap();
+                    let entry = dict.get(&id).unwrap();
+                    let variant = &entry.variants[variant_index];
+                    println!(
+                        "{}\t{}\t({})",
+                        variant.word,
+                        pr_to_string(&variant.prs[pr_index]),
+                        score
+                    );
+                    i += 1;
+                }
+                println!("\n----\n");
+            });
         }
     }
 }
