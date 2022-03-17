a74758fb3cb756fb39ac6e20b7d0592965980fad
Kevin Li <kevinli020508@gmail.com>
2022-03-17 14:11:08 -0400

    Move and rename all.csv in examples/parse_dict

--- examples/parse_dict/src/main.rs
+++ examples/parse_dict/src/main.rs
@@ -1,7 +1,7 @@
 use wordshk_tools::parse::parse_dict;
 
 fn main() {
-    static DATA_FILE: &'static str = include_str!("../all.csv");
+    static DATA_FILE: &'static str = include_str!("../../wordshk.csv");
     let dict = parse_dict(DATA_FILE.as_bytes());
     println!("{:?}", dict);
 }
