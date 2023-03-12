f4ab57f3662c9338889f5dc29bd1dc9d09a24e7c
Kevin Li <kevinli020508@gmail.com>
2023-03-12 11:55:35 -0400

    Show colloquial words in parse_dict example

--- examples/parse_dict/src/main.rs
+++ examples/parse_dict/src/main.rs
@@ -1,9 +1,47 @@
-use wordshk_tools::dict::Entry;
+use wordshk_tools::dict::{Dict, Entry};
 use wordshk_tools::parse::parse_dict;
 
 fn main() {
     static DATA_FILE: &'static str = include_str!("../../wordshk.csv");
     let dict = parse_dict(DATA_FILE.as_bytes()).expect("Failed to parse dict");
+    show_colloquial_words(&dict);
+}
+
+fn show_colloquial_words(dict: &Dict) {
+    let mut words = std::collections::HashSet::new();
+    for entry in dict.values() {
+        if entry.labels.contains(&"書面語".to_string())
+            || entry.labels.contains(&"專名".to_string())
+            || entry.labels.contains(&"大陸".to_string())
+            || entry.labels.contains(&"術語".to_string())
+            || entry.labels.contains(&"錯字".to_string())
+            || entry.labels.contains(&"文言".to_string())
+            || entry.labels.contains(&"舊式".to_string())
+            || is_offensive(entry)
+        {
+            continue;
+        }
+        if entry.labels.contains(&"口語".to_string()) || entry.labels.contains(&"潮語".to_string())
+        {
+            for word in entry.variants.0.iter().map(|variant| &variant.word) {
+                words.insert((word, true));
+            }
+        } else {
+            for word in entry.variants.0.iter().map(|variant| &variant.word) {
+                words.insert((word, false));
+            }
+        }
+    }
+    for (word, is_colloquial) in words {
+        if is_colloquial {
+            println!("{}\tcolloquial", word);
+        } else {
+            println!("{}", word);
+        }
+    }
+}
+
+fn show_offensive(dict: &Dict) {
     // Show offensive words in the dictionary
     let mut offensive_words = vec![];
     'outer: for entry in dict.values() {
