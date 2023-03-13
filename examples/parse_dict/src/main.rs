e24b554d2c58ca05152d102752f1a7654e98b2aa
Kevin Li <kevinli020508@gmail.com>
2023-03-13 07:53:51 -0400

    Add POS tag to output

--- examples/parse_dict/src/main.rs
+++ examples/parse_dict/src/main.rs
@@ -1,5 +1,6 @@
 use wordshk_tools::dict::{Dict, Entry};
 use wordshk_tools::parse::parse_dict;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     static DATA_FILE: &'static str = include_str!("../../wordshk.csv");
@@ -8,7 +9,8 @@ fn main() {
 }
 
 fn show_colloquial_words(dict: &Dict) {
-    let mut words = std::collections::HashSet::new();
+    let mut words = HashSet::new();
+    // let mut poses = HashSet::new();
     for entry in dict.values() {
         if entry.labels.contains(&"書面語".to_string())
             || entry.labels.contains(&"專名".to_string())
@@ -21,20 +23,62 @@ fn show_colloquial_words(dict: &Dict) {
         {
             continue;
         }
+        // poses.insert(entry.poses.first().unwrap().clone());
         if entry.labels.contains(&"口語".to_string()) || entry.labels.contains(&"潮語".to_string())
         {
-            words.insert((get_variants(&entry), true));
+            words.insert((
+                get_variants(&entry),
+                get_pos(&entry),
+                true,
+            ));
         } else {
-            words.insert((get_variants(&entry), false));
+            words.insert((
+                get_variants(&entry),
+                get_pos(&entry),
+                false,
+            ));
         }
     }
-    for (word, is_colloquial) in words {
+    for (word, pos, is_colloquial) in words {
         if is_colloquial {
-            println!("{}\tcolloquial", word.join(","));
+            println!("{}\t{}\tcolloquial", word.join(","), pos);
         } else {
-            println!("{}", word.join(","));
+            println!("{}\t{}", word.join(","), pos);
         }
     }
+    // for pos in poses {
+    //     println!("{}", pos);
+    // }
+}
+
+fn get_pos(entry: &Entry) -> &'static str {
+    convert_zh_pos_to_en(entry.poses.first().unwrap())
+}
+
+// https://gist.github.com/hscspring/c985355e0814f01437eaf8fd55fd7998
+// Convert words.hk POS tag to Jieba standard
+fn convert_zh_pos_to_en(zh_pos: &str) -> &'static str {
+    let map: HashMap<&str, &str> = HashMap::from_iter([
+        ("方位詞", "f"),
+        ("動詞", "v"),
+        ("擬聲詞", "o"),
+        ("感嘆詞", "e"),
+        ("代詞", "r"),
+        ("副詞", "d"),
+        ("詞綴", "?"),
+        ("名詞", "n"),
+        ("連詞", "c"),
+        ("量詞", "q"),
+        ("助詞", "u"),
+        ("區別詞", "b"),
+        ("數詞", "m"),
+        ("形容詞", "a"),
+        ("介詞", "p"),
+        ("語句", "l"),
+        ("語素", "g"),
+    ]);
+
+    map.get(zh_pos).unwrap()
 }
 
 fn show_offensive(dict: &Dict) {
@@ -70,5 +114,10 @@ fn is_offensive(entry: &Entry) -> bool {
 }
 
 fn get_variants(entry: &Entry) -> Vec<String> {
-    entry.variants.0.iter().map(|variant| variant.word.clone()).collect()
+    entry
+        .variants
+        .0
+        .iter()
+        .map(|variant| variant.word.clone())
+        .collect()
 }
