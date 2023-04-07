51b5dde705de5c859a33b8fe2d289da65927ba4d
Kevin Li <kevinli020508@gmail.com>
2023-04-07 19:00:23 -0400

    Add example for printing all english definition of words

--- examples/parse_dict/src/main.rs
+++ examples/parse_dict/src/main.rs
@@ -1,11 +1,43 @@
-use wordshk_tools::dict::{Dict, Entry};
-use wordshk_tools::parse::parse_dict;
 use std::collections::{HashMap, HashSet};
+use wordshk_tools::dict::{clause_to_string, Dict, Entry};
+use wordshk_tools::parse::parse_dict;
 
 fn main() {
     static DATA_FILE: &'static str = include_str!("../../wordshk.csv");
     let dict = parse_dict(DATA_FILE.as_bytes()).expect("Failed to parse dict");
-    show_colloquial_words(&dict);
+    // show_colloquial_words(&dict);
+    // show_tagged_words(&dict, &["爭議"]);
+    get_eng_definitions(&dict);
+}
+
+fn get_eng_definitions(dict: &Dict) {
+    use itertools::Itertools;
+    use std::fs::File;
+    use std::io::BufWriter;
+    use std::io::Write;
+
+    let mut defs: HashMap<String, HashSet<String>> = HashMap::new();
+    for entry in dict.values() {
+        let variants: String = entry
+            .variants
+            .0
+            .iter()
+            .map(|variant| variant.word.clone())
+            .join("/");
+        for def in &entry.defs {
+            if let Some(eng) = &def.eng {
+                defs.entry(variants.clone())
+                    .and_modify(|defs| {
+                        defs.insert(clause_to_string(&eng));
+                    })
+                    .or_insert(HashSet::from([clause_to_string(&eng)]));
+            }
+        }
+    }
+    let file = File::create("eng_definitions.json").unwrap();
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &defs).unwrap();
+    writer.flush().unwrap();
 }
 
 fn show_colloquial_words(dict: &Dict) {
@@ -19,24 +51,16 @@ fn show_colloquial_words(dict: &Dict) {
             || entry.labels.contains(&"錯字".to_string())
             || entry.labels.contains(&"文言".to_string())
             || entry.labels.contains(&"舊式".to_string())
-            || is_offensive(entry)
+            || has_tag(entry, &["黃賭毒", "粗俗"])
         {
             continue;
         }
         // poses.insert(entry.poses.first().unwrap().clone());
         if entry.labels.contains(&"口語".to_string()) || entry.labels.contains(&"潮語".to_string())
         {
-            words.insert((
-                get_variants(&entry),
-                get_pos(&entry),
-                true,
-            ));
+            words.insert((get_variants(&entry), get_pos(&entry), true));
         } else {
-            words.insert((
-                get_variants(&entry),
-                get_pos(&entry),
-                false,
-            ));
+            words.insert((get_variants(&entry), get_pos(&entry), false));
         }
     }
     for (word, pos, is_colloquial) in words {
@@ -81,11 +105,11 @@ fn convert_zh_pos_to_en(zh_pos: &str) -> &'static str {
     map.get(zh_pos).unwrap()
 }
 
-fn show_offensive(dict: &Dict) {
+fn show_tagged_words(dict: &Dict, tags: &[&'static str]) {
     // Show offensive words in the dictionary
-    let mut offensive_words = vec![];
+    let mut tagged_words = vec![];
     'outer: for entry in dict.values() {
-        if is_offensive(entry) {
+        if has_tag(entry, &tags) {
             for word in get_variants(&entry) {
                 // Filter words that are ambiguously used as a non-offensive word
                 for other_entry in dict.values() {
@@ -95,22 +119,22 @@ fn show_offensive(dict: &Dict) {
                             .0
                             .iter()
                             .any(|variant| variant.word == *word)
-                        && !is_offensive(other_entry)
                     {
                         continue 'outer;
                     }
                 }
-                offensive_words.push(word.clone());
+                tagged_words.push(word.clone());
             }
         }
     }
-    for word in offensive_words {
+    for word in tagged_words {
         println!("{}", word);
     }
 }
 
-fn is_offensive(entry: &Entry) -> bool {
-    entry.labels.contains(&"黃賭毒".to_string()) || entry.labels.contains(&"粗俗".to_string())
+fn has_tag(entry: &Entry, tags: &[&'static str]) -> bool {
+    tags.iter()
+        .any(|tag| entry.labels.contains(&tag.to_string()))
 }
 
 fn get_variants(entry: &Entry) -> Vec<String> {
