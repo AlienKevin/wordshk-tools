0383936fa7c4780111a81b656a89b9235e1eb2ce
Kevin Li <kevinli020508@gmail.com>
2024-01-09 11:16:59 -0500

    Add try embeddings scripts

---
+++ examples/export_api/try_minilm_embeddings.py
@@ -0,0 +1,29 @@
+from sentence_transformers import SentenceTransformer
+
+defs = [
+    "everyone",
+    "everybody",
+    "everybody, often excluding the speaker when used without other personal pronouns",
+    "everyone; without exception",
+    "not everything; not everyone; it depends",
+    "everyone has his own opinion; everyone talking at once; literally: seven mouths eight tongues",
+    "as everyone knows; to be known to all",
+    "everyone's hand",
+    "the whole school; everyone from the school",
+    "an expression meaning something is obvious or self-evident; no doubt; everyone knows this; literally: no need to ask Mr Gwai",
+    "praised by everyone"
+]
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
+embeddings = model.encode(defs)
+
+# Rank the cosine similarity between the first def and the rest
+# and print out the ranked list of defs
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+cosine_similarities = cosine_similarity([embeddings[0]], embeddings[1:])[0]
+ranked_indices = np.argsort(cosine_similarities)[::-1]
+for i in ranked_indices:
+    print(defs[i+1])
+
