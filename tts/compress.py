c601c1f1c2379f7f3b4d1fd0ff8a9749fe9f5749
Kevin Li <kevinli020508@gmail.com>
2024-07-11 07:07:18 +0800

    Compress to mp3 instead of m4a for wider compatability

--- tts/compress.py
+++ tts/compress.py
@@ -11,11 +11,14 @@ import concurrent.futures
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     futures = []
     for filename in os.listdir('audio'):
-        if filename.endswith(".m4a") and not os.path.exists(os.path.join(audio_compressed_dir, filename)):
+        if filename.endswith(".m4a"):
             input_file_path = os.path.join('audio', filename)
             output_file_path = os.path.join(audio_compressed_dir, os.path.splitext(filename)[0] + '.mp3')
-            command = f'ffmpeg -i {input_file_path} -codec:a libmp3lame -qscale:a 2 {output_file_path}'
-            futures.append(executor.submit(lambda cmd: subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL), command))
+            if not os.path.exists(output_file_path):
+                command = f'ffmpeg -i {input_file_path} -codec:a libmp3lame -qscale:a 2 {output_file_path}'
+                futures.append(executor.submit(lambda cmd: subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL), command))
     
     for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Compressing audio files"):
         pass
+
+    print(f"Number of generated compressed files: {len(futures)}")
