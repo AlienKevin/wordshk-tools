e38c9ddf75e5e73a37bb59ad0b4d45e2b3d7b9fa
Kevin Li <kevinli020508@gmail.com>
2024-07-11 07:07:34 +0800

    Only upload files last modified one hour ago

--- tts/upload_to_bucket.py
+++ tts/upload_to_bucket.py
@@ -1,15 +1,19 @@
-import oss2
 from get_bucket import get_bucket
 from tqdm import tqdm
 import os
+import time
 
 bucket = get_bucket()
-
 def upload_audios():
+    uploaded_count = 0
+    cutoff_time = time.time() - 1 * 60 * 60  # Only upload files modified within 1 hours ago
     for file in tqdm(os.listdir('audio_compressed')):
         file_path = os.path.join('audio_compressed', file)
-        with open(file_path, 'rb') as fileobj:
-            bucket.put_object(file, fileobj)
+        if os.path.getmtime(file_path) > cutoff_time:
+            with open(file_path, 'rb') as fileobj:
+                bucket.put_object(file, fileobj)
+                uploaded_count += 1
+    print(f'Number of audios uploaded: {uploaded_count}')
 
 # Run the function to upload .m4a files
 upload_audios()
