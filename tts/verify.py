10e1232e4eb0d6dff4635cd8e7f049018582b0ef
Kevin Li <kevinli020508@gmail.com>
2024-07-11 07:06:59 +0800

    Verify if any audio file is missing in audio/ instead of checking the audio_compressed/ dir

--- tts/verify.py
+++ tts/verify.py
@@ -4,18 +4,6 @@ import hashlib
 from tts import extract_egs
 
 audio_dir = "audio/"
-compressed_dir = "audio_compressed/"
-
-# Get the list of audio files in the audio directory
-audio_files = os.listdir(audio_dir)
-
-# Get the list of compressed audio files
-compressed_files = os.listdir(compressed_dir)
-
-# Find the missing audio files
-missing_files = set(audio_files) - set(compressed_files)
-
-print(f"Missing {len(missing_files)} audio files in {compressed_dir}")
 
 # Load the data from dict.json
 with open('../dict.json', 'r') as file:
@@ -23,15 +11,16 @@ with open('../dict.json', 'r') as file:
 
 egs = extract_egs(data)
 
-# Compare the hash of egs with the missing_files' names
-for eg in egs.keys():
-    eg_hash = hashlib.sha256(eg.encode()).hexdigest() + ".m4a"
-    if eg_hash in missing_files:
-        print(f"Missing eg: {eg}, hash: {eg_hash}")
+# Verify all egs are generated in audio/
 
-# Delete those missing audios from audio_dir
+# Get the list of audio files in the audio directory
+audio_files = os.listdir(audio_dir)
+
+# Find the missing egs
+missing_egs = []
 for eg in egs.keys():
     eg_hash = hashlib.sha256(eg.encode()).hexdigest() + ".m4a"
-    if eg_hash in missing_files:
-        os.remove(os.path.join(audio_dir, eg_hash))
-        print(f"Deleted missing eg: {eg}, hash: {eg_hash}")
+    if eg_hash not in audio_files:
+        missing_egs.append(eg)
+
+print(f"Missing {len(missing_egs)} egs in {audio_dir}")
