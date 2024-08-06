604b03e52542298c37a811ede1f2ddf813f494d7
Kevin Li <kevinli020508@gmail.com>
2024-08-06 10:58:36 +0800

    Support different configurations in visualize_pos_erros.py

--- genai/visualize_pos_errors.py
+++ genai/visualize_pos_errors.py
@@ -1,10 +1,18 @@
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, choices=['deepseek-chat', 'deepseek-coder', 'gpt-4o', 'gpt-4o-mini', 'qwen-max', 'qwen-plus', 'qwen-turbo'], required=True, help='Model to use for POS tagging')
+parser.add_argument('--prompt_version', type=str, choices=['v1', 'v2'], required=True, help='Prompt version to use for POS tagging')
+parser.add_argument('--eval_dataset', type=str, choices=['hkcancor', 'ud_yue'], required=True, help='Dataset to evaluate POS tagging on')
+parser.add_argument('--segmentation_given', type=bool, default=False, help='Whether the segmentation is given')
+args = parser.parse_args()
 
 # Read the error data
-with open('outputs/hkcancor_pos_tagged_errors.jsonl', 'r', encoding='utf-8') as error_file:
+with open(f'outputs/pos_errors_{args.eval_dataset}_{args.model}_prompt_{args.prompt_version}{"_segmentation_given" if args.segmentation_given else ""}.jsonl', 'r', encoding='utf-8') as error_file:
     error_data = [json.loads(line) for line in error_file]
 
-with open('outputs/hkcancor_pos_tagged.jsonl', 'r', encoding='utf-8') as file:
+with open(f'outputs/pos_{args.eval_dataset}_{args.model}_prompt_{args.prompt_version}{"_segmentation_given" if args.segmentation_given else ""}.jsonl', 'r', encoding='utf-8') as file:
     data = [json.loads(line) for line in file]
 
 import difflib
@@ -42,7 +50,7 @@ for entry in error_data:
     reference_sentence = "".join(reference_tokens)
     hypothesis_sentence = "".join(hypothesis_tokens)
     
-    diff = get_edits_string(reference_sentence, hypothesis_sentence)
+    diff = get_edits_string(hypothesis_sentence, reference_sentence)
     
     print(diff)
 
@@ -56,6 +64,6 @@ for entry in data:
         reference_sentence = " ".join(reference_tokens)
         hypothesis_sentence = " ".join(hypothesis_tokens)
         
-        diff = get_edits_string(reference_sentence, hypothesis_sentence)
+        diff = get_edits_string(hypothesis_sentence, reference_sentence)
         
         print(diff)
