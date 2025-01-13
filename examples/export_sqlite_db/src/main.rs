945d5383d415500f9a4a704eaf811ef96c17a289
Kevin Li <kevinli020508@gmail.com>
2025-01-12 23:07:33 -0800

    Bump sqlite db's app version number

--- examples/export_sqlite_db/src/main.rs
+++ examples/export_sqlite_db/src/main.rs
@@ -24,7 +24,7 @@ fn export_sqlite_db() {
     if std::fs::metadata(&dict_path).is_ok() {
         std::fs::remove_file(&dict_path).unwrap();
     }
-    api.export_dict_as_sqlite_db(&dict_path, "3.4.0+33")
+    api.export_dict_as_sqlite_db(&dict_path, "3.4.1+34")
         .unwrap();
 }
 
